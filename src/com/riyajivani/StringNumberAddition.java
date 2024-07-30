d2b3b24b50faf2b84f91f300c3a487f2fc91cffe
Riya Jivani <riyajivani8@gmail.com>
2024-07-30 20:00:04 +0530

    Negative number will be printed in exception.

--- src/com/riyajivani/StringNumberAddition.java
+++ src/com/riyajivani/StringNumberAddition.java
@@ -14,37 +14,44 @@ public class StringNumberAddition {
 
     public int addition(String numbers){
         
+        //if string is empty then addition should 0.
         if(numbers.isEmpty()){
             return 0;
         }
         
+        //replace all non-numeric characters with comma.
         numbers = numbers.replaceAll("[^0-9-]+", ",");
         
+        //check whether string is empty or not after replacement.
         if(numbers.replace(",", "").isEmpty()){
             return 0;
         }
         
+        //storing comma seperated numbers into string array.
         String[] nums = numbers.split(",");
-        int addition = 0;
         
+        int addition = 0;
         List<String> negativenums = new ArrayList<>();
         
+        //adding non-negative numbers.
         for(String num : nums){
             if(!num.isEmpty()){
                 if(Integer.parseInt(num)<0){
                     negativenums.add(num);
                 }else{
-                  addition += Integer.parseInt(num);
+                    addition += Integer.parseInt(num);
                 } 
             }
         }
         
+        
+        //if presence of negative number, throw exception.
         if(!negativenums.isEmpty()){
-            throw new IllegalArgumentException("Negative numbers not allowed.");
+            throw new IllegalArgumentException("Negative numbers not allowed, " + String.join(", " ,negativenums));
         }
         else
         {
-        return addition;
+            return addition;
         }
-    }    
+    } 
 }
--- test/com/riyajivani/StringNumberAdditionTest.java
+++ test/com/riyajivani/StringNumberAdditionTest.java
@@ -17,13 +17,21 @@ public class StringNumberAdditionTest {
     @Test
     public void testAddition() {
         StringNumberAddition addition = new StringNumberAddition();
+        
         assertEquals(0,addition.addition(""));
+        
         assertEquals(1,addition.addition("1"));
+        
         assertEquals(3, addition.addition("1,2"));
+        
         assertEquals(8, addition.addition("3\n5"));
+        
         assertEquals(13, addition.addition("2@6\\5"));
+        
         assertEquals(12, addition.addition("010,2,#"));
+        
         assertEquals(36, addition.addition("$(,6;0,12)        18"));
+        
         assertEquals(0, addition.addition("Shruti Bhalodia"));
     }
     
@@ -31,10 +39,10 @@ public class StringNumberAdditionTest {
     public void negativeNumTestAddition(){
         StringNumberAddition addition = new StringNumberAddition();
         try{
-            addition.addition("-7,3,6\\r");
+            addition.addition("-7,-3,6\\r");
         }catch(IllegalArgumentException e){
-            assertThat(e.getMessage(),is("Negative numbers not allowed."));
+            assertThat(e.getMessage(),is("Negative numbers not allowed, -7, -3"));
         }
-}
+    }
     
 }
