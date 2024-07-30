321f452c1f3d0e2c95f04a0a6d8946d14b1020b3
Riya Jivani <riyajivani8@gmail.com>
2024-07-30 18:39:52 +0530

    First testcase passed that is 0 should be returned for empty string.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Incubyte-TDD-Assessment
+This Repository contains the code to find the addition of numbers from given string, and also various testcases.
---
+++ test/com/riyajivani/StringNumberAdditionTest.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package com.riyajivani;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author riyaj
+ */
+public class StringNumberAdditionTest {
+    
+    @Test
+    public void testAddition() {
+        StringNumberAddition addition = new StringNumberAddition();
+        assertEquals(0,addition.addition(""));
+    }
+    
+}
